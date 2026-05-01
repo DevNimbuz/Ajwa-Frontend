25f2d3a8355be26c9ea3f3d52529ea1a608bc175
Shamal Khalid N P <shamalkhalidnp@gmail.com>
2026-05-01 22:00:56 +0530

    fixed errors in ui

--- src/components/admin/DashboardCharts.jsx
+++ src/components/admin/DashboardCharts.jsx
@@ -10,7 +10,7 @@ import {
 export const FunnelChart = ({ data }) => {
   return (
     <div style={{ width: '100%', height: 300 }}>
-      <ResponsiveContainer>
+      <ResponsiveContainer width="100%" height="100%">
         <BarChart data={data} layout="vertical" margin={{ top: 5, right: 30, left: 40, bottom: 5 }}>
           <CartesianGrid strokeDasharray="3 3" stroke="#334155" horizontal={false} />
           <XAxis type="number" hide />
@@ -45,7 +45,7 @@ export const DevicePieChart = ({ data }) => {
   
   return (
     <div style={{ width: '100%', height: 260 }}>
-      <ResponsiveContainer>
+      <ResponsiveContainer width="100%" height="100%">
         <PieChart>
           <Pie
             data={data}
@@ -77,7 +77,7 @@ export const DevicePieChart = ({ data }) => {
 export const TrendChart = ({ data }) => {
   return (
     <div style={{ width: '100%', height: 300 }}>
-      <ResponsiveContainer>
+      <ResponsiveContainer width="100%" height="100%">
         <AreaChart data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
           <defs>
             <linearGradient id="colorValue" x1="0" y1="0" x2="0" y2="1">
